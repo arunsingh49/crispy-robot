1d9a781e36f14b42561b6b78cd8d8398b822322c
Arun Singh <arunsingh.49@gmail.com>
2020-08-18 10:54:47 +1000

    setting up CD

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ require('./startup/db')();
 require('./startup/validate')();
 require('./startup/routes')(app);
 
+// setup some production file serving
+if (['production'].includes(process.env.NODE_ENV)) {
+	app.use(express.static('client/build'));
+
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve('client', 'website', 'build', 'index.html'));
+	});
+}
+
 const port = process.env.PORT || config.get('defaultPort');
 const server = app.listen(port, () => {
 	winston.info(`App listening on ${port}`);
